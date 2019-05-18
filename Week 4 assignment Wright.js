b09ae37a2f3ebce9ded0a258493c448988a88be3
pwright117 <50474799+pwright117@users.noreply.github.com>
2019-05-18 16:33:11 -0700

    Create Week 4 assignment Wright.js

---
+++ Week 4 assignment Wright.js
@@ -0,0 +1,21 @@
+let test_set = [1,2,3], new_set;
+
+function myset_add(data, new_value) { ... }
+
+new_set = myset_add(test_set, 3);
+if ( ! arrays_equal(new_set.sort(), [1,2,3]) ) {
+  console.log("Failed adding existing data");
+}
+
+new_set = myset_add(test_set, 4);
+if ( ! arrays_equal(new_set.sort(), [1,2,3,4]) ) {
+  console.log("Failed adding new data");
+}
+
+function myset_remove(data, remove_value) { ... }
+
+test_set = [1,2,3];
+new_set = myset_remove(test_set, 3);
+if ( ! arrays_equal(new_set.sort(), [1,2]) ) {
+  console.log("Remove failed");
+}

ed51cc144054694a4d8b19237aa791f959046ac6
pwright117 <50474799+pwright117@users.noreply.github.com>
2019-05-20 17:47:12 -0700

    After gaining direction

--- Week 4 assignment Wright.js
+++ Week 4 assignment Wright.js
@@ -1,27 +1,16 @@
-let test_set = [1,2,3], new_set;
-
-function myset_add(data, new_value) { ... }
-
-new_set = myset_add(test_set, 3);
-if ( ! arrays_equal(new_set.sort(), [1,2,3]) ) {
-  console.log("Failed adding existing data");
+function afunc(myArray, newValue) {
+  for ( let i of myArray ) {
+    if (myArray[i] === newValue) {
+      result=myArray
+      break
+    } else {
+    if (i=(myArray.length-1)) {
+      result = myArray.concat(newValue)
+    } else {
+      continue
+    }
+    }
+  } return result
 }
 
-new_set = myset_add(test_set, 4);
-if ( ! arrays_equal(new_set.sort(), [1,2,3,4]) ) {
-  console.log("Failed adding new data");
-}
-
-function myset_remove(data, remove_value) { ... }
-
-test_set = [1,2,3];
-new_set = myset_remove(test_set, 3);
-if ( ! arrays_equal(new_set.sort(), [1,2]) ) {
-  console.log("Remove failed");
-}
-
-
-// Return true if test_value is in the set, false otherwise
-function myset_has(data, test_value) { ... }
-
-function myset_size(data) { ... }
+console.log(afunc([1,2,3,4,5],10))
